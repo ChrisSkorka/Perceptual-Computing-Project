d36fca8be6d17833e07095c259dac1f68ec15709
Christopher Skorka <30539703+TechSupport420@users.noreply.github.com>
2018-09-19 12:52:18 +1000

    Started corner detection

--- py/dewrap.py
+++ py/dewrap.py
@@ -21,11 +21,33 @@ def processImageFile(filename):
 
 	# read file and convert to grey scale
 	imgOriginal = cv2.imread(filename)
-	imgOriginalGrey = img1 = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
+	imgOriginalGrey = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
 
+	# find the relevant corners
+	findCorners(imgOriginalGrey)
 
 	# show
 	cv2.imshow('imgOriginalGrey', imgOriginalGrey)
+	cv2.waitKey(10000)
+
+# find corners in image and finds the four page corners
+# parameters: 	imgGrey:			grey scale image
+# returns:		( (x, y), ... ):	four corners that are the page
+def findCorners(imgGrey):
+	imgGrey = imgGrey.copy()
+	imgGrey = np.float32(imgGrey)
+
+	dst = cv2.cornerHarris(imgGrey, 2, 5, 0.01)
+
+	#result is dilated for marking the corners, not important
+	dst = cv2.dilate(dst, None)
+
+	# Threshold for an optimal value, it may vary depending on the image.
+	imgColor = cv2.cvtColor(np.uint8(imgGrey), cv2.COLOR_GRAY2BGR)
+	imgColor[dst > 0.01 * dst.max()] = [0,0,255]
+
+	cv2.imshow('corners', imgColor)
+
 
 # run main program
 if __name__ == "__main__":
