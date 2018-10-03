fc74610681c9d48ac1f9340052257a0f9bb25d2d
Christopher Skorka <30539703+TechSupport420@users.noreply.github.com>
2018-10-03 12:21:32 +1000

    corner detection experiments

--- py/dewrap.py
+++ py/dewrap.py
@@ -37,34 +37,39 @@ def processImageFile(filename):
 # parameters: 	imgGrey:			grey scale image
 # returns:		( (x, y), ... ):	four corners that are the page
 def findCorners(imgOriginal):
-	# imgGrey = imgGrey.copy()
-	# imgGrey = np.float32(imgGrey)
-	# imgColor = cv2.cvtColor(np.uint8(imgGrey), cv2.COLOR_GRAY2BGR)
+	imgGrey = cv2.cvtColor(np.uint8(imgOriginal), cv2.COLOR_BGR2GRAY)
+	imgGrey = np.float32(imgGrey)
+	imgColor = imgOriginal
 	cv2.imshow('orb', imgOriginal)
 	img = np.copy(imgOriginal)
+	# cv2.waitKey(10000)
+
+	# # Initiate ORB detector
+	# orb = cv2.ORB_create()
+	# # find the keypoints with ORB
+	# kp = orb.detect(img, None)
+	# # compute the descriptors with ORB
+	# kp, des = orb.compute(img, kp)
+	# # draw only keypoints location,not size and orientation
+	# img2 = cv2.drawKeypoints(img, kp, None, color = (0, 0, 255), flags = 5)
+	# cv2.imshow('orb', img)
+
+
+	dst = cv2.cornerHarris(imgGrey, 20, 15, 0.01)
+
+	#result is dilated for marking the corners, not important
+	dst2 = cv2.dilate(dst, None)
+
+	# Threshold for an optimal value, it may vary depending on the image.
+	imgColor1 = imgColor.copy()
+	imgColor1[dst > 0.01 * dst.max()] = [0,0,255]
+	cv2.imshow('corners', imgColor1)
+
+	imgColor2 = imgColor.copy()
+	imgColor2[dst > 0.01 * dst.max()] = [0,0,255]
+	cv2.imshow('corners2', imgColor2)
+
 	cv2.waitKey(10000)
-
-	# Initiate ORB detector
-	orb = cv2.ORB_create()
-	# find the keypoints with ORB
-	kp = orb.detect(img, None)
-	# compute the descriptors with ORB
-	kp, des = orb.compute(img, kp)
-	# draw only keypoints location,not size and orientation
-	img2 = cv2.drawKeypoints(img, kp, None, color = (0, 0, 255), flags = 5)
-	cv2.imshow('orb', img)
-
-
-	# dst = cv2.cornerHarris(imgGrey, 2, 5, 0.01)
-
-	# #result is dilated for marking the corners, not important
-	# dst = cv2.dilate(dst, None)
-
-	# # Threshold for an optimal value, it may vary depending on the image.
-	# imgColor[dst > 0.01 * dst.max()] = [0,0,255]
-
-	# cv2.imshow('corners', imgColor)
-
 
 # run main program
 if __name__ == "__main__":
